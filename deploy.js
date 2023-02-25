6fc6c824886bb9cdcd9b54d0b4261ea9248020bb
apinanyogaratnam <apinanapinan@icloud.com>
2023-02-25 09:47:23 -0500

    fix: deploy script

--- deploy.js
+++ deploy.js
@@ -3,10 +3,11 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 const { abi, evm } = require('./build/NFT.json');
+const { exit } = require('process');
 
 const provider = new HDWalletProvider(
-  [`0x${process.env.MNEMONIC}`],
-  'https://goerli.infura.io/v3/7228b494adf94a63bab07ea737183033'
+  [process.env.PRIVATE_KEY],
+  process.env.INFURA_URL,
 );
 
 const web3 = new Web3(provider);
@@ -14,11 +15,18 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
+  const balance = await web3.eth.getBalance(accounts[0]);
+  console.log('balance of account', web3.utils.fromWei(balance, 'ether'));
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: [] })
-    .send({ gas: '10000000', from: accounts[0] });
+  try {
+    const result = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.object, arguments: [] })
+      .send({ gas: '10000000', from: accounts[0], gasPrice: '1000000000' });
+  } catch (error) {
+    console.log(error);
+    exit();
+  }
 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
